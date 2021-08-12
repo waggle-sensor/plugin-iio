2036df014058e5eb68e79c26bda9cea23a8ddb71
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-08-12 12:03:39 -0500

    fixed sensor detection and added unit test

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ import logging
 import waggle.plugin as plugin
 
 
-def build_iio_param_list():
+def build_iio_param_list(rootdir):
     param_list = []
-    for sensor_path in Path("/sys/bus/iio/devices").glob("*/name"):
-        sensor_name = sensor_path.read_text().strip()
+    for name_path in Path(rootdir).glob("*/name"):
+        sensor_name = name_path.read_text().strip()
+        sensor_path = name_path.parent
+
         for path in sensor_path.glob("in_*_input"):
             name = path.name.strip()
-            param_list.append((sensor_name, name, path))    
+            param_list.append((sensor_name, name, path))
+
     return param_list
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--rootdir", type=Path, default="/sys/bus/iio/devices", help="root iio device directory")
     parser.add_argument("--debug", action="store_true", help="enable debug logs")
     parser.add_argument("--rate", default=30.0, type=float, help="sampling rate")
     args = parser.parse_args()
@@ -25,10 +29,11 @@ def main():
                         format="%(asctime)s %(message)s",
                         datefmt="%Y/%m/%d %H:%M:%S")
 
-    param_list = build_iio_param_list()
+    param_list = build_iio_param_list(args.rootdir)
+    print(param_list)
 
     for sensor_name, name, _ in param_list:
-        logging.info("detected parameter %s %s", sensor_name, name)
+        logging.info("detected sensor %r param %r", sensor_name, name)
 
     logging.info("will scan and publish iio sensor values every %ss", args.rate)
 
