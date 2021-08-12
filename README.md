20d0112a05e0e35b4859aba3579e51e4ee9af518
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-08-12 11:47:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IIO Plugin
+
+This plugin scans for all Linux IIO subsystem sensors on a device a reports their values.
---
+++ main.py
@@ -0,0 +1,44 @@
+import argparse
+from pathlib import Path
+import time
+import logging
+import waggle.plugin as plugin
+
+
+def build_iio_param_list():
+    param_list = []
+    for sensor_path in Path("/sys/bus/iio/devices").glob("*/name"):
+        sensor_name = sensor_path.name.strip()
+        for path in sensor_path.glob("in_*_input"):
+            name = path.read_text().strip()
+            param_list.append((sensor_name, name, path))    
+    return param_list
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="enable debug logs")
+    parser.add_argument("--rate", default=30.0, type=float, help="sampling rate")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
+                        format="%(asctime)s %(message)s",
+                        datefmt="%Y/%m/%d %H:%M:%S")
+
+    param_list = build_iio_param_list()
+
+    for sensor_name, name, _ in param_list:
+        logging.info("detected parameter %s %s", sensor_name, name)
+
+    logging.info("will scan and publish iio sensor values every %ss", args.rate)
+
+    plugin.init()
+
+    while True:
+        time.sleep(args.rate)
+        for sensor_name, name, path in detected_paths:
+            value = float(path.read_text())
+            plugin.publish(f"iio.{name}", value, meta={"sensor": sensor_name})
+
+if __name__ == "__main__":
+    main()
