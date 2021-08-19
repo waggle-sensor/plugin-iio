922630153c8beb7bd17729c87d313705319a7e91
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-08-19 11:34:44 -0500

    updated test suite to include real examples and naming. added filter arg.

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from pathlib import Path
 import time
 import logging
 import waggle.plugin as plugin
+import re
 
 
-def build_iio_param_list(rootdir):
-    param_list = []
+def build_iio_param_set(rootdir, filter=""):
+    filterRE = re.compile(filter)
+
+    results = set()
+
     for name_path in Path(rootdir).glob("*/name"):
         sensor_name = name_path.read_text().strip()
+        if not filterRE.search(sensor_name):
+            continue
         sensor_path = name_path.parent
-
         for path in sensor_path.glob("in_*_input"):
             name = path.name.strip()
-            param_list.append((sensor_name, name, path))
+            results.add((sensor_name, name, path))
 
-    return param_list
+    return results
 
 
 def main():
@@ -24,15 +29,16 @@ def main():
     parser.add_argument("--debug", action="store_true", help="enable debug logs")
     parser.add_argument("--rate", default=30.0, type=float, help="sampling rate")
     parser.add_argument("--scope", default="all", choices=["all", "node", "beehive"], help="publish scope")
+    parser.add_argument("--filter", default="", help="filter sensor name")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
                         format="%(asctime)s %(message)s",
                         datefmt="%Y/%m/%d %H:%M:%S")
 
-    param_list = build_iio_param_list(args.rootdir)
+    param_set = build_iio_param_set(args.rootdir, args.filter)
 
-    for sensor_name, name, _ in param_list:
+    for sensor_name, name, _ in param_set:
         logging.info("detected sensor %r param %r", sensor_name, name)
 
     logging.info("will scan and publish iio sensor values every %ss", args.rate)
@@ -41,8 +47,15 @@ def main():
 
     while True:
         time.sleep(args.rate)
-        for sensor_name, name, path in param_list:
-            value = float(path.read_text())
+        for sensor_name, name, path in param_set:
+            text = path.read_text()
+
+            try:
+                value = float(text)
+            except ValueError:
+                logging.info("failed to parse %s %s data %s as numeric", sensor_name, name, text)
+                continue
+
             plugin.publish(f"iio.{name}", value, meta={"sensor": sensor_name}, scope=args.scope)
             logging.debug("published %s %s %s", sensor_name, name, value)
 
--- test.py
+++ test.py
@@ -1,20 +1,49 @@
 import unittest
-from main import build_iio_param_list
+from main import build_iio_param_set
 from pathlib import Path
 
 
 class TestUtils(unittest.TestCase):
 
     def test_build_iio_param_list(self):
-        param_list = build_iio_param_list("test/sys/bus/iio/devices")
+        param_list = build_iio_param_set("test/sys/bus/iio/devices")
+        expected = {
+            ("ina3221x", "in_voltage0_input", Path("test/sys/bus/iio/devices/iio:device0/in_voltage0_input")),
+            ("ina3221x", "in_current1_input", Path("test/sys/bus/iio/devices/iio:device0/in_current1_input")),
+            ("ina3221x", "in_voltage2_input", Path("test/sys/bus/iio/devices/iio:device0/in_voltage2_input")),
+            ("ina3221x", "in_current_sum_input", Path("test/sys/bus/iio/devices/iio:device0/in_current_sum_input")),
+            ("ina3221x", "in_power1_trigger_input", Path("test/sys/bus/iio/devices/iio:device0/in_power1_trigger_input")),
+            ("ina3221x", "in_power2_trigger_input", Path("test/sys/bus/iio/devices/iio:device0/in_power2_trigger_input")),
+            ("ina3221x", "in_current0_input", Path("test/sys/bus/iio/devices/iio:device0/in_current0_input")),
+            ("ina3221x", "in_current0_trigger_input", Path("test/sys/bus/iio/devices/iio:device0/in_current0_trigger_input")),
+            ("ina3221x", "in_current2_input", Path("test/sys/bus/iio/devices/iio:device0/in_current2_input")),
+            ("ina3221x", "in_voltage1_input", Path("test/sys/bus/iio/devices/iio:device0/in_voltage1_input")),
+            ("ina3221x", "in_current2_trigger_input", Path("test/sys/bus/iio/devices/iio:device0/in_current2_trigger_input")),
+            ("ina3221x", "in_power0_input", Path("test/sys/bus/iio/devices/iio:device0/in_power0_input")),
+            ("ina3221x", "in_power2_input", Path("test/sys/bus/iio/devices/iio:device0/in_power2_input")),
+            ("ina3221x", "in_current1_trigger_input", Path("test/sys/bus/iio/devices/iio:device0/in_current1_trigger_input")),
+            ("ina3221x", "in_power0_trigger_input", Path("test/sys/bus/iio/devices/iio:device0/in_power0_trigger_input")),
+            ("ina3221x", "in_power1_input", Path("test/sys/bus/iio/devices/iio:device0/in_power1_input")),
 
-        expected = [
-            ('test1', 'in_temperature_input', Path('test/sys/bus/iio/devices/iio:1/in_temperature_input')),
-            ('test1', 'in_pressure_input', Path('test/sys/bus/iio/devices/iio:1/in_pressure_input')),
-            ('test2', 'in_resistance_input', Path('test/sys/bus/iio/devices/iio:2/in_resistance_input')),
-        ]
+            ("bme280", "in_humidityrelative_input", Path("test/sys/bus/iio/devices/iio:device1/in_humidityrelative_input")),
+            ("bme280", "in_pressure_input", Path("test/sys/bus/iio/devices/iio:device1/in_pressure_input")),
+            ("bme280", "in_temp_input", Path("test/sys/bus/iio/devices/iio:device1/in_temp_input")),
 
-        self.assertEqual(sorted(param_list), sorted(expected))
+            ("bme680", "in_humidityrelative_input", Path("test/sys/bus/iio/devices/iio:device2/in_humidityrelative_input")),
+            ("bme680", "in_resistance_input", Path("test/sys/bus/iio/devices/iio:device2/in_resistance_input")),
+            ("bme680", "in_pressure_input", Path("test/sys/bus/iio/devices/iio:device2/in_pressure_input")),
+            ("bme680", "in_temp_input", Path("test/sys/bus/iio/devices/iio:device2/in_temp_input")),
+        }
+        self.assertEqual(param_list, expected)
+    
+    def test_build_iio_param_list_filter(self):
+        param_list = build_iio_param_set("test/sys/bus/iio/devices", "bme280")
+        expected = {
+            ("bme280", "in_humidityrelative_input", Path("test/sys/bus/iio/devices/iio:device1/in_humidityrelative_input")),
+            ("bme280", "in_pressure_input", Path("test/sys/bus/iio/devices/iio:device1/in_pressure_input")),
+            ("bme280", "in_temp_input", Path("test/sys/bus/iio/devices/iio:device1/in_temp_input")),
+        }
+        self.assertEqual(param_list, expected)
 
 
 if __name__ == "__main__":
